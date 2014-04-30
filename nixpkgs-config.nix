272b4297e1e01adf8413b51012f7d97d48523dcf
Corey O'Connor <coreyoconnor@gmail.com>
2014-04-29 18:03:32 -0700

    use on disk wine source

--- nixpkgs-config.nix
+++ nixpkgs-config.nix
@@ -9,9 +9,7 @@
 
       wine = pkgs.stdenv.lib.overrideDerivation pkgs.wineUnstable (oldAttrs: {
         name = "wine-head";
-        src = pkgs.fetchgit {
-          url = "git@github.com:coreyoconnor/wine.git";
-        };
+        src = ../wine;
       });
   };
 }

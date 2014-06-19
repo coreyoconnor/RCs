5630deec95057d2235764a6fa0643e8344a0c6de
Corey O'Connore <ococorey@lab126.com>
2014-06-19 10:14:50 -0700

    more emacs packages

---
+++ emacs.d/elpa/haskell-mode-20140619.139/haskell-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "haskell-mode" "20140619.139" "A Haskell editing mode" 'nil)
---
+++ emacs.d/elpa/omniref-20140222.1226/omniref-pkg.el
@@ -0,0 +1,1 @@
+(define-package "omniref" "20140222.1226" "Omniref Ruby documentation search engine interface" (quote nil))

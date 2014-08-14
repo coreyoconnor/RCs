8ea14cf4721c2f61eb6342a597b593066b9042ca
Corey O'Connor <ococorey@lab126.com>
2014-08-14 13:40:43 -0700

    emacs updates

--- emacs.d/elpa/haskell-mode-20140805.158/haskell-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "haskell-mode" "20140805.158" "A Haskell editing mode" 'nil)
--- emacs.d/elpa/robe-20140724.2137/robe-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "robe" "20140724.2137" "Code navigation, documentation lookup and completion for Ruby"
-  '((inf-ruby "2.3.0"))
-  :url "https://github.com/dgutov/robe" :keywords
-  '("ruby" "convenience" "rails"))

1d7a6d20b3922195abcfdeb6db336de116dc64d9
Corey O'Connor <ococorey@lab126.com>
2014-09-25 13:57:37 -0700

    emacs updates

---
+++ emacs.d/elpa/robe-20140915.1700/robe-pkg.el
@@ -0,0 +1,4 @@
+(define-package "robe" "20140915.1700" "Code navigation, documentation lookup and completion for Ruby"
+  '((inf-ruby "2.3.0"))
+  :url "https://github.com/dgutov/robe" :keywords
+  '("ruby" "convenience" "rails"))

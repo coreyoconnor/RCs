9f85614808fcf9776ef1808421117ce05d0b669f
Corey O'Connor <coreyoconnor@gmail.com>
2014-10-03 22:20:41 -0700

    nixpkgs-config changes

--- nixpkgs-config.nix
+++ nixpkgs-config.nix
@@ -3,11 +3,7 @@
   allowUnfree = true;
 
   packageOverrides = pkgs: {
-      kde4 = pkgs.kde412;
-      freetype = import (pkgs.path + "/pkgs/development/libraries/freetype") {
-          inherit (pkgs) stdenv fetchurl gnumake;
-          useInfinality = true;
-      };
+      kde4 = pkgs.kde414;
 
       wine = pkgs.misc.debugVersion (pkgs.stdenv.lib.overrideDerivation pkgs.wineUnstable (oldAttrs: {
         name = "wine-head";

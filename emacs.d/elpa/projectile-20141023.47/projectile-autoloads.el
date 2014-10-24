6befbf87664132cc02ed68bd80cdc26572a306e5
Corey O'Connor <ococorey@lab126.com>
2014-10-24 14:54:12 -0700

    emacs updates

--- emacs.d/elpa/projectile-20141023.47/projectile-autoloads.el
+++ emacs.d/elpa/projectile-20141023.47/projectile-autoloads.el
@@ -4,8 +4,8 @@
 
 
 ;;;### (autoloads (projectile-global-mode projectile-mode projectile-mode-line
-;;;;;;  projectile-commander) "projectile" "projectile.el" (21575
-;;;;;;  61237 0 0))
+;;;;;;  projectile-commander) "projectile" "projectile.el" (21578
+;;;;;;  51789 0 0))
 ;;; Generated autoloads from projectile.el
 
 (autoload 'projectile-commander "projectile" "\
@@ -70,7 +70,7 @@ See `projectile-mode' for more information on Projectile mode.
 
 ;;;***
 
-;;;### (autoloads nil nil ("projectile-pkg.el") (21575 61237 483188
+;;;### (autoloads nil nil ("projectile-pkg.el") (21578 51789 968581
 ;;;;;;  0))
 
 ;;;***

215014988f3aef9413ba5555d1bd906e2b34c6f5
Corey O'Connor <ococorey@lab126.com>
2014-12-03 12:54:22 -0800

    update emacs packages

---
+++ emacs.d/elpa/popup-20141120.1742/popup-autoloads.el
@@ -0,0 +1,18 @@
+;;; popup-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("popup-pkg.el" "popup.el") (21631 30759
+;;;;;;  666652 0))
+
+;;;***
+
+(provide 'popup-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; popup-autoloads.el ends here

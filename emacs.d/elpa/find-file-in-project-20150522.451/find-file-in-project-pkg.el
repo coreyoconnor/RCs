b397908417a266613e94ed3e77e924b73febd94d
Corey O'Connor <coreyoconnor@gmail.com>
2015-06-04 11:10:09 -0700

    emacs updates

--- emacs.d/elpa/find-file-in-project-20150522.451/find-file-in-project-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "find-file-in-project" "20150522.451" "Find files in a project quickly." 'nil :url "http://www.emacswiki.org/cgi-bin/wiki/FindFileInProject" :keywords '("project" "convenience"))

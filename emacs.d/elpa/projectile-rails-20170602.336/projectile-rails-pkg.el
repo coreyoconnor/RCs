3beff8b38a8a0de04a2267cc4f0701be017307f3
Corey O'Connor <ococorey@amazon.com>
2017-06-27 23:51:13 +0000

    ~

---
+++ emacs.d/elpa/projectile-rails-20170602.336/projectile-rails-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "projectile-rails" "20170602.336" "Minor mode for Rails projects based on projectile-mode" '((emacs "24.3") (projectile "0.12.0") (inflections "1.1") (inf-ruby "2.2.6") (f "0.13.0") (rake "0.3.2")) :commit "4e864c75bfbee4a5a1a9c76d3d2a77b2da01fa5f" :url "https://github.com/asok/projectile-rails" :keywords '("rails" "projectile"))

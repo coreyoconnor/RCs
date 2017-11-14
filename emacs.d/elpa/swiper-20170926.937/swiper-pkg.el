adbc64b9c1daa2d70f7cd3793e185e5ae85723e3
Corey O'Connor <coreyoconnor@gmail.com>
2017-11-13 23:46:52 -0800

    ~

--- emacs.d/elpa/swiper-20170926.937/swiper-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "swiper" "20170926.937" "Isearch with an overview. Oh, man!" '((emacs "24.1") (ivy "0.9.0")) :commit "8268b2267399fdc06438b6969df3de59e65f2d7d" :url "https://github.com/abo-abo/swiper" :keywords '("matching"))

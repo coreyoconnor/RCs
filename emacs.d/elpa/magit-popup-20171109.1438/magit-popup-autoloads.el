6a731c5cc735a066fa2a00f9e7b4f6cfc39a824b
Corey O'Connor <coreyoconnor@gmail.com>
2017-11-24 14:50:41 -0800

    ~

--- emacs.d/elpa/magit-popup-20171109.1438/magit-popup-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; magit-popup-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("magit-popup-pkg.el" "magit-popup.el")
-;;;;;;  (23050 40238 939557 752000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; magit-popup-autoloads.el ends here
--- emacs.d/elpa/projectile-rails-20171103.1004/projectile-rails-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "projectile-rails" "20171103.1004" "Minor mode for Rails projects based on projectile-mode" '((emacs "24.3") (projectile "0.12.0") (inflections "1.1") (inf-ruby "2.2.6") (f "0.13.0") (rake "0.3.2")) :commit "ff5f63df4e1403f5ab1286f1099a072a5f6694d9" :url "https://github.com/asok/projectile-rails" :keywords '("rails" "projectile"))

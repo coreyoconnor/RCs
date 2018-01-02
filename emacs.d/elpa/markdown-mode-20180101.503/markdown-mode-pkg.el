7f89e1a6a51e71d05ae2679925c337796afc0e3c
Corey O'Connor <coreyoconnor@gmail.com>
2018-01-02 15:14:21 -0800

    ~

---
+++ emacs.d/elpa/markdown-mode-20180101.503/markdown-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "markdown-mode" "20180101.503" "Major mode for Markdown-formatted text" '((emacs "24.4") (cl-lib "0.5")) :commit "fc4e143831f8590dc9b25da76961536eade02411" :url "https://jblevins.org/projects/markdown-mode/" :keywords '("markdown" "github flavored markdown" "itex"))
--- emacs.d/elpa/swiper-20171217.334/swiper-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "swiper" "20171217.334" "Isearch with an overview. Oh, man!" '((emacs "24.1") (ivy "0.9.0")) :commit "88218154604f9fc0bb618c0953d0ce057812f5ed" :url "https://github.com/abo-abo/swiper" :keywords '("matching"))

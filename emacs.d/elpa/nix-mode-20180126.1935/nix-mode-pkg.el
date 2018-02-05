3c185d92bb0142c6d7f68de7afd2596711a07535
Corey O'Connor <coreyoconnor@gmail.com>
2018-02-05 15:50:55 -0800

    ~

---
+++ emacs.d/elpa/nix-mode-20180126.1935/nix-mode-pkg.el
@@ -0,0 +1,7 @@
+(define-package "nix-mode" "20180126.1935" "Major mode for editing .nix files"
+  '((emacs "24.3"))
+  :url "https://github.com/matthewbauer/nix-mode" :keywords
+  '("nix" "languages" "tools" "unix"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:

7cab7d0bebf62317b990a3fa16beb426c59bfa4c
Corey O'Connor <coreyoconnor@gmail.com>
2018-03-06 16:03:55 -0800

    ~

---
+++ emacs.d/elpa/nix-mode-20180215.1331/nix-mode-pkg.el
@@ -0,0 +1,7 @@
+(define-package "nix-mode" "20180215.1331" "Major mode for editing .nix files"
+  '((emacs "24.3"))
+  :url "https://github.com/NixOS/nix-mode" :keywords
+  '("nix" "languages" "tools" "unix"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ emacs.d/elpa/rake-20180212.208/rake-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "rake" "20180212.208" "Run rake commands" '((f "0.13.0") (dash "1.5.0") (cl-lib "0.5")) :commit "9c204334b03b4e899fadae6e59c20cf105404128" :url "https://github.com/asok/rake.el" :keywords '("rake" "ruby"))

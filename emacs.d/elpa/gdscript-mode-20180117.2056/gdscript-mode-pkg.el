3989706f29af3ea19a96ac31e5820b3dd208bbe5
Corey O'Connor <coreyoconnor@gmail.com>
2018-04-09 14:19:45 -0700

    ~

---
+++ emacs.d/elpa/gdscript-mode-20180117.2056/gdscript-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "gdscript-mode" "20180117.2056" "Major mode for editing Godot GDScript files" '((emacs "24.3")) :commit "31af5283eaec207bc864022a28e2824132471eaf" :keywords '("languages") :url "https://github.com/AdamBark/gdscript-mode")
---
+++ emacs.d/elpa/projectile-rails-20180327.433/projectile-rails-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "projectile-rails" "20180327.433" "Minor mode for Rails projects based on projectile-mode" '((emacs "24.3") (projectile "0.12.0") (inflections "1.1") (inf-ruby "2.2.6") (f "0.13.0") (rake "0.3.2")) :commit "b5e516ef6a336bde32e4594408be15fd96b07e1b" :keywords '("rails" "projectile") :url "https://github.com/asok/projectile-rails")

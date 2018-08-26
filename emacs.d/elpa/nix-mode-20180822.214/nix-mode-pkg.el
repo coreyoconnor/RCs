f6a813d33c35e675fcc4b6475c3907f6af7d750f
Corey O'Connor <coreyoconnor@gmail.com>
2018-08-25 20:18:50 -0700

    ~

---
+++ emacs.d/elpa/nix-mode-20180822.214/nix-mode-pkg.el
@@ -0,0 +1,10 @@
+(define-package "nix-mode" "20180822.214" "Major mode for editing .nix files"
+  '((emacs "24.3"))
+  :keywords
+  '("nix" "languages" "tools" "unix")
+  :maintainer
+  '("Matthew Bauer" . "mjbauer95@gmail.com")
+  :url "https://github.com/NixOS/nix-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ emacs.d/elpa/swiper-20180813.1625/swiper-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "swiper" "20180813.1625" "Isearch with an overview. Oh, man!" '((emacs "24.1") (ivy "0.9.0")) :commit "02537c95baf183b6a42e142a85742d589c692aa2" :keywords '("matching") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/swiper")

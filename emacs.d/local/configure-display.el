78c31fb3a66680d67884b6c7500560d9f35caac3
Corey O'Connor <coreyoconnor@gmail.com>
2018-11-07 23:36:00 -0800

    ~

--- emacs.d/local/configure-display.el
+++ emacs.d/local/configure-display.el
@@ -64,39 +64,37 @@
             )
   )
 
+; from: https://zuttobenkyou.wordpress.com/2012/06/15/emacs-vimlike-tabwindow-navigation/
+(defun vimlike-quit (old-fun &rest args)
+  (interactive)
+  (let ((one-elscreen (elscreen-one-screen-p))
+        (one-window (one-window-p))
+        )
+    (cond
+        ; if current tab has split windows in it, close the current live window
+        ((not one-window)
+        (delete-window) ; delete the current window
+        ; (balance-windows) ; balance remaining windows
+        nil)
+        ; if there are multiple elscreens (tabs), close the current elscreen
+        ((not one-elscreen)
+        (elscreen-kill)
+        nil)
+        ; if there is only one elscreen, just try to quit (calling elscreen-kill
+        ; will not work, because elscreen-kill fails if there is only one
+        ; elscreen)
+        (one-elscreen
+        (evil-quit)
+        nil)
+        )))
+
 (use-package elscreen :ensure t
   :after (:all evil evil-tabs)
   :config (progn
             (elscreen-start)
             ;; (require 'evil-elscreen)
 
-            ; from: https://zuttobenkyou.wordpress.com/2012/06/15/emacs-vimlike-tabwindow-navigation/
-            (evil-define-command vimlike-quit ()
-                "Vimlike ':q' behavior: close current window if there are split windows;
-                otherwise, close current tab (elscreen)."
-                (interactive)
-                (let ((one-elscreen (elscreen-one-screen-p))
-                        (one-window (one-window-p))
-                        )
-                    (cond
-                    ; if current tab has split windows in it, close the current live window
-                    ((not one-window)
-                    (delete-window) ; delete the current window
-                    ; (balance-windows) ; balance remaining windows
-                    nil)
-                    ; if there are multiple elscreens (tabs), close the current elscreen
-                    ((not one-elscreen)
-                    (elscreen-kill)
-                    nil)
-                    ; if there is only one elscreen, just try to quit (calling elscreen-kill
-                    ; will not work, because elscreen-kill fails if there is only one
-                    ; elscreen)
-                    (one-elscreen
-                    (evil-quit)
-                    nil)
-                    )))
-
-            (evil-ex-define-cmd "q[uit]" 'vimlike-quit)
+            (advice-add #'evil-tab-sensitive-quit' :around #'vimlike-quit)
             (add-hook 'elscreen-goto-hook (lambda () (set-window-vscroll nil 0)))
             )
   )

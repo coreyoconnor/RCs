cdc65293241291823324b13f3cc6cc9d3deeaca2
Corey O'Connor <coreyoconnor@gmail.com>
2019-03-12 10:45:03 -0700

    ~

---
+++ emacs.d/elpa/color-theme-sanityinc-solarized-20190206.59/color-theme-sanityinc-solarized-pkg.el
@@ -0,0 +1,12 @@
+(define-package "color-theme-sanityinc-solarized" "20190206.59" "A version of Ethan Schoonover's Solarized themes"
+  '((cl-lib "0.6"))
+  :keywords
+  '("faces" "themes")
+  :authors
+  '(("Steve Purcell" . "steve@sanityinc.com"))
+  :maintainer
+  '("Steve Purcell" . "steve@sanityinc.com")
+  :url "http://github.com/purcell/color-theme-sanityinc-solarized")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ emacs.d/elpa/proof-general-20190212.1433/proof-general-pkg.el
@@ -0,0 +1,5 @@
+(define-package "proof-general" "20190212.1433" "A generic front-end for proof assistants (interactive theorem provers)"
+  '((emacs "24.3")))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:

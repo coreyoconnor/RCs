8688df064f2a8e0580a6d61476972e22a62c85b7
Corey O'Connor <coreyoconnor@gmail.com>
2019-04-22 12:52:42 -0700

    ~

---
+++ emacs.d/elpa/color-theme-20190220.1115/color-theme-pkg.el
@@ -0,0 +1,10 @@
+(define-package "color-theme" "20190220.1115" "An OBSOLETE color-theme implementation" 'nil :keywords
+  '("faces")
+  :authors
+  '(("Jonadab the Unsightly One" . "jonadab@bright.net"))
+  :maintainer
+  '("Xavier Maillard" . "zedek@gnu.org")
+  :url "http://www.emacswiki.org/cgi-bin/wiki.pl?ColorTheme")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:

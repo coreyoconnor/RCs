a44813c5688ec03d8d37c1d165ec05d7f6c3a915
Corey O'Connor <coreyoconnor@gmail.com>
2019-08-05 10:56:38 -0700

    ~

---
+++ emacs.d/elpa/scad-mode-20190413.1246/scad-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "scad-mode" "20190413.1246" "A major mode for editing OpenSCAD code" 'nil :commit "e731c04b696754c0e24ba3c4707422978034615a" :keywords '("languages") :authors '(("Len Trigg, Łukasz Stelmach")) :maintainer '("Len Trigg" . "lenbok@gmail.com") :url "https://raw.github.com/openscad/openscad/master/contrib/scad-mode.el")

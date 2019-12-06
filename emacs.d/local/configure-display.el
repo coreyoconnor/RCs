004c8aa061b6b6200044b493d0fabf1d2f785d46
Corey O'Connor <coreyoconnor@gmail.com>
2019-12-06 13:50:42 -0800

    use centaur-tabs to avoid screen corruption with elscreen

--- emacs.d/local/configure-display.el
+++ emacs.d/local/configure-display.el
@@ -26,46 +26,59 @@
 (setq frame-title-format
       (concat  "%b - emacs@" (system-name)))
 
-(use-package evil-tabs :ensure t
-  :after (:all evil)
-  :config (progn
-            (define-key evil-normal-state-map "gt" 'elscreen-next)
-            (define-key evil-normal-state-map "gT" 'elscreen-previous)
-            )
-  )
+(when nil
+  (use-package evil-tabs :ensure t
+    :after (:all evil)
+    :config (progn
+              (define-key evil-normal-state-map "gt" 'elscreen-next)
+              (define-key evil-normal-state-map "gT" 'elscreen-previous)
+              )
+    )
 
-; from: https://zuttobenkyou.wordpress.com/2012/06/15/emacs-vimlike-tabwindow-navigation/
-(defun vimlike-quit (old-fun &rest args)
-  (interactive)
-  (let ((one-elscreen (elscreen-one-screen-p))
-        (one-window (one-window-p))
-        )
-    (cond
-        ; if current tab has split windows in it, close the current live window
-        ((not one-window)
+                                        ; from: https://zuttobenkyou.wordpress.com/2012/06/15/emacs-vimlike-tabwindow-navigation/
+  (defun vimlike-quit (old-fun &rest args)
+    (interactive)
+    (let ((one-elscreen (elscreen-one-screen-p))
+          (one-window (one-window-p))
+          )
+      (cond
+                                        ; if current tab has split windows in it, close the current live window
+       ((not one-window)
         (delete-window) ; delete the current window
-        ; (balance-windows) ; balance remaining windows
+                                        ; (balance-windows) ; balance remaining windows
         nil)
-        ; if there are multiple elscreens (tabs), close the current elscreen
-        ((not one-elscreen)
+                                        ; if there are multiple elscreens (tabs), close the current elscreen
+       ((not one-elscreen)
         (elscreen-kill)
         nil)
-        ; if there is only one elscreen, just try to quit (calling elscreen-kill
-        ; will not work, because elscreen-kill fails if there is only one
-        ; elscreen)
-        (one-elscreen
+                                        ; if there is only one elscreen, just try to quit (calling elscreen-kill
+                                        ; will not work, because elscreen-kill fails if there is only one
+                                        ; elscreen)
+       (one-elscreen
         (evil-quit)
         nil)
-        )))
+       )))
 
-(use-package elscreen :ensure t
-  :after (:all evil evil-tabs)
+
+  (use-package elscreen :ensure t
+    :after (:all evil evil-tabs)
+    :config (progn
+              (elscreen-start)
+              (advice-add #'evil-tab-sensitive-quit' :around #'vimlike-quit)
+              (add-hook 'elscreen-goto-hook (lambda () (set-window-vscroll nil 0)))
+              )
+    )
+  )
+
+(use-package centaur-tabs
+  :ensure t
+  :after (:all evil)
   :config (progn
-            (elscreen-start)
-            ;; (require 'evil-elscreen)
-
-            (advice-add #'evil-tab-sensitive-quit' :around #'vimlike-quit)
-            (add-hook 'elscreen-goto-hook (lambda () (set-window-vscroll nil 0)))
+            (setq centaur-tabs-cycle-scope 'tabs)
+            (centaur-tabs-mode t)
+            (define-key evil-normal-state-map "gt" 'centaur-tabs-forward)
+            (define-key evil-normal-state-map "gT" 'centaur-tabs-backward)
+            (evil-ex-define-cmd "quit" 'kill-buffer)
             )
   )
 

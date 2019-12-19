2da7e186ec26cb9f339964623ec401a6caae7edd
Corey O'Connor <coreyoconnor@gmail.com>
2019-12-18 17:08:12 -0800

    ~

--- emacs.d/elpa/moody-20191105.2245/moody-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "moody" "20191105.2245" "Tabs and ribbons for the mode line" '((emacs "25.3")) :commit "b51c2218fd1d207186694cae7d09315795f3c847" :authors '(("Jonas Bernoulli" . "jonas@bernoul.li")) :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :url "https://github.com/tarsius/moody")

504e8327a1216ea05c500f860b82115daeba31f6
Corey O'Connor <coreyoconnor@gmail.com>
2020-01-13 13:56:51 -0800

    ~

--- emacs.d/elpa/moody-20191211.2031/moody-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "moody" "20191211.2031" "Tabs and ribbons for the mode line" '((emacs "25.3")) :commit "b93430766ad523ec36ff1528fb3e659af7b7dd49" :authors '(("Jonas Bernoulli" . "jonas@bernoul.li")) :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :url "https://github.com/tarsius/moody")
---
+++ emacs.d/elpa/moody-20200103.1238/moody-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "moody" "20200103.1238" "Tabs and ribbons for the mode line" '((emacs "25.3")) :commit "3616a6bd7c6b028753dc11feabd14597da694a63" :authors '(("Jonas Bernoulli" . "jonas@bernoul.li")) :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :url "https://github.com/tarsius/moody")

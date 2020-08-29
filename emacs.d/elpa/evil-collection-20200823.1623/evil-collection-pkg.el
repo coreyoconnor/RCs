f65b6256550dd892914cff4e9bb6f38a973c13a0
Corey O'Connor <coreyoconnor@gmail.com>
2020-08-28 19:02:17 -0700

    ~

---
+++ emacs.d/elpa/evil-collection-20200823.1623/evil-collection-pkg.el
@@ -0,0 +1,14 @@
+(define-package "evil-collection" "20200823.1623" "A set of keybindings for Evil mode"
+  '((emacs "25.1")
+    (evil "1.2.13")
+    (annalist "1.0"))
+  :commit "567ee24d443906fbcd8b192f8505a3b4b0ee3129" :keywords
+  '("evil" "tools")
+  :authors
+  '(("James Nguyen" . "james@jojojames.com"))
+  :maintainer
+  '("James Nguyen" . "james@jojojames.com")
+  :url "https://github.com/emacs-evil/evil-collection")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ emacs.d/elpa/gdscript-mode-20200828.1445/gdscript-mode-pkg.el
@@ -0,0 +1,12 @@
+(define-package "gdscript-mode" "20200828.1445" "Major mode for Godot's GDScript language"
+  '((emacs "26.3"))
+  :commit "949ac8c47a3039c07d79f75dd852b12d04aa4d0d" :keywords
+  '("languages")
+  :authors
+  '(("Nathan Lovato <nathan@gdquest.com>, Fabián E. Gallina" . "fgallina@gnu.org"))
+  :maintainer
+  '(nil . "nathan@gdquest.com")
+  :url "https://github.com/GDQuest/emacs-gdscript-mode/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:

77dc9420ac1acd5ca4eb9c7e688252ae9b81b654
Corey O'Connor <coreyoconnor@gmail.com>
2021-05-14 23:10:56 +0000

    ~

---
+++ emacs.d/elpa/centaur-tabs-20210507.1633/centaur-tabs-pkg.el
@@ -0,0 +1,12 @@
+(define-package "centaur-tabs" "20210507.1633" "Aesthetic, modern looking customizable tabs plugin"
+  '((emacs "24.4")
+    (powerline "2.4")
+    (cl-lib "0.5"))
+  :commit "9c7c936e4e1de6f4f4095d70e43c9ae738d05086" :authors
+  '(("Emmanuel Bustos" . "ema2159@gmail.com"))
+  :maintainer
+  '("Emmanuel Bustos" . "ema2159@gmail.com")
+  :url "https://github.com/ema2159/centaur-tabs")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ emacs.d/elpa/evil-collection-20210507.1741/evil-collection-pkg.el
@@ -0,0 +1,14 @@
+(define-package "evil-collection" "20210507.1741" "A set of keybindings for Evil mode"
+  '((emacs "25.1")
+    (evil "1.2.13")
+    (annalist "1.0"))
+  :commit "23c60d576cf2ca4e931ca43815ceccc3f98acec1" :authors
+  '(("James Nguyen" . "james@jojojames.com"))
+  :maintainer
+  '("James Nguyen" . "james@jojojames.com")
+  :keywords
+  '("evil" "tools")
+  :url "https://github.com/emacs-evil/evil-collection")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:

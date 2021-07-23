8fc787b605afb1e890c269a22c7ed0f41860500c
Corey O'Connor <coreyoconnor@gmail.com>
2021-07-23 22:08:21 +0000

    ~

---
+++ emacs.d/elpa/evil-collection-20210721.1454/evil-collection-pkg.el
@@ -0,0 +1,14 @@
+(define-package "evil-collection" "20210721.1454" "A set of keybindings for Evil mode"
+  '((emacs "25.1")
+    (evil "1.2.13")
+    (annalist "1.0"))
+  :commit "8d10ad4ea59752595de37a8ddc44e8d3fb14fe89" :authors
+  '(("James Nguyen" . "james@jojojames.com"))
+  :maintainer
+  '("James Nguyen" . "james@jojojames.com")
+  :keywords
+  '("evil" "tools")
+  :url "https://github.com/emacs-evil/evil-collection")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:

3c4aa9b02a232990464de774c26a61347d77377b
Corey O'Connor <coreyconnor@gmail.com>
2021-09-06 21:14:38 -0700

    ~

---
+++ emacs.d/elpa/ansible-doc-20160924.824/ansible-doc-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from ansible-doc-20160924.824/ansible-doc.el  -*- no-byte-compile: t -*-
+(define-package "ansible-doc" "20160924.824" "Ansible documentation Minor Mode" '((emacs "24.3")) :commit "86083a7bb2ed0468ca64e52076b06441a2f8e9e0" :authors '(("Sebastian Wiesner" . "swiesner@lunaryorn")) :maintainer '("Sebastian Wiesner" . "swiesner@lunaryorn") :keywords '("tools" "help") :url "https://github.com/lunaryorn/ansible-doc.el")
---
+++ emacs.d/elpa/mmm-jinja2-20170313.1420/mmm-jinja2-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from mmm-jinja2-20170313.1420/mmm-jinja2.el  -*- no-byte-compile: t -*-
+(define-package "mmm-jinja2" "20170313.1420" "MMM submode class for Jinja2 Templates" '((mmm-mode "0.5.4")) :commit "c8cb763174fa2fb61b9a0e5e0ff8cb0210f8492f" :authors '(("Ben Hayden" . "hayden767@gmail.com")) :maintainer '("Ben Hayden" . "hayden767@gmail.com") :url "https://github.com/glynnforrest/mmm-jinja2")

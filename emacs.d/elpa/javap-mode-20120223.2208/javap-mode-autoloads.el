f60cb1bc0cc8f9d3b2f8683c2657dd13ebeae568
Corey O'Connor <coreyoconnor@gmail.com>
2021-12-24 06:36:50 +0000

    ~

---
+++ emacs.d/elpa/javap-mode-20120223.2208/javap-mode-autoloads.el
@@ -0,0 +1,27 @@
+;;; javap-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "javap-mode" "javap-mode.el" (0 0 0 0))
+;;; Generated autoloads from javap-mode.el
+
+(autoload 'javap-mode "javap-mode" "\
+A major mode for viewing javap files.
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "javap-mode" '("javap-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; javap-mode-autoloads.el ends here

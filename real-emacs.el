16028ed91e77e5197631198556ec1e7433ec3813
Corey O'Connor <coreyoconnor@gmail.com>
2022-11-06 23:53:24 +0000

    use https

--- real-emacs.el
+++ real-emacs.el
@@ -41,13 +41,13 @@
 
 (defun configure-package-repos ()
   (add-to-list 'package-archives
-               '("gnu" . "http://elpa.gnu.org/packages/"))
+               '("gnu" . "https://elpa.gnu.org/packages/"))
   (add-to-list 'package-archives
-               '("org" . "http://orgmode.org/elpa/"))
+               '("org" . "https://orgmode.org/elpa/"))
   (add-to-list 'package-archives
-               '("melpa" . "http://melpa.org/packages/"))
+               '("melpa" . "https://melpa.org/packages/"))
   (add-to-list 'package-archives
-               '("melpa-stable" . "http://stable.melpa.org/packages/"))
+               '("melpa-stable" . "https://stable.melpa.org/packages/"))
 
   (setq package-enable-at-startup nil)
   )

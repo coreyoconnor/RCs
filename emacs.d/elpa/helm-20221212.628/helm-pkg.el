d4d0a391e1a8d7643d622e7964cfa02211b57cf3
Corey O'Connor <coreyoconnor@gmail.com>
2022-12-14 20:42:40 -0800

    ~

---
+++ emacs.d/elpa/helm-20221212.628/helm-pkg.el
@@ -0,0 +1,11 @@
+(define-package "helm" "20221212.628" "Helm is an Emacs incremental and narrowing framework"
+  '((helm-core "3.9.0")
+    (popup "0.5.3"))
+  :commit "e81fbbc687705595ab65ae5cd3bdf93c17a90743" :authors
+  '(("Thierry Volpiatto" . "thievol@posteo.net"))
+  :maintainer
+  '("Thierry Volpiatto" . "thievol@posteo.net")
+  :url "https://emacs-helm.github.io/helm/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:

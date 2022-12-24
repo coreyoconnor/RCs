c7eb3d25001efa9764fed235e5216bbc71314537
Corey O'Connor <coreyoconnor@gmail.com>
2022-12-24 15:20:12 -0800

    ~

---
+++ emacs.d/elpa/ac-php-20210820.1000/ac-php-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from ac-php.el  -*- no-byte-compile: t -*-
+(define-package "ac-php" "20210820.1000" "Auto Completion source for PHP." '((ac-php-core "2.0") (auto-complete "1.4.0") (yasnippet "0.8.0")) :commit "e29075f810af73f6bf7803eebf15d96bffee7154" :authors '(("jim" . "xcwenn@qq.com")) :maintainer '("jim") :keywords '("completion" "convenience" "intellisense") :url "https://github.com/xcwen/ac-php")
---
+++ emacs.d/elpa/web-mode-20221224.1731/web-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from web-mode.el  -*- no-byte-compile: t -*-
+(define-package "web-mode" "20221224.1731" "major mode for editing web templates" '((emacs "23.1")) :commit "18e4ef2061e12fc4cc96fdc2caf2760a85dec248" :authors '(("François-Xavier Bois")) :maintainer '("François-Xavier Bois" . "fxbois@gmail.com") :keywords '("languages") :url "https://web-mode.org")

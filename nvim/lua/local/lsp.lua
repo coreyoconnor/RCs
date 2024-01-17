284f11ff25cf6d7a1f4c7317b646d55cabaddb96
Corey O'Connor <corey.oconnor@protenus.com>
2024-01-17 08:24:49 -0800

    shut off metals status bar provider

--- nvim/lua/local/lsp.lua
+++ nvim/lua/local/lsp.lua
@@ -117,7 +117,7 @@ local setup = function()
   end
 
   local metals_config = require("metals").bare_config()
-  metals_config.init_options.statusBarProvider = "on"
+  -- metals_config.init_options.statusBarProvider = "on"
   metals_config.settings.showImplicitArguments = true
   metals_config.tvp["icons"] = { enabled = true }
   metals_config.capabilities = require("cmp_nvim_lsp").default_capabilities()

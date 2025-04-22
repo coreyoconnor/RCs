c9e2bce173d3e77ec7a84c230e47a57668e6f041
Corey O'Connor <coreyoconnor@gmail.com>
2025-04-22 09:30:45 -0700

    ~

--- nvim/init.lua
+++ nvim/init.lua
@@ -100,7 +100,16 @@ require("lazy").setup({
               fallback()
             end
           end,
-        }
+        },
+        enabled = function()
+          local disabled = false
+          disabled = disabled or (vim.api.nvim_buf_get_option(0, "buftype") == "prompt")
+          disabled = disabled or (vim.fn.reg_recording() ~= "")
+          disabled = disabled or (vim.fn.reg_executing() ~= "")
+          disabled = disabled or require("cmp.config.context").in_treesitter_capture("comment")
+          disabled = disabled or require"cmp.config.context".in_syntax_group("Comment")
+          return not disabled
+        end
       }
     end
   },
